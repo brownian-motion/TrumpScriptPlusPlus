b7bd812dd0da981c4385fe776fd2f15257e8f234
JJ Brown <jack.brown.255@gmail.com>
2018-03-05 20:18:51 -0600

    Documentation

--- src/main/java/com/brownian/trumpscript/tokenizer/TokenizerDemo.java
+++ src/main/java/com/brownian/trumpscript/tokenizer/TokenizerDemo.java
@@ -6,6 +6,10 @@ import com.brownian.trumpscript.tokenizer.token.Token;
 
 import java.io.*;
 
+/**
+ * The MAIN class for the {@link SCANNER}. Demonstrates the functionality of the tokenizer module according
+ * to the specifications of the first project.
+ */
 public class TokenizerDemo {
     private static final String TOKENIZER_TEST_SCRIPT_RESOURCE_NAME = "TokenizerTestScript.trump";
 
@@ -17,6 +21,11 @@ public class TokenizerDemo {
         tokenizeTestScriptAndPrintTokensAndSymbolTable();
     }
 
+    /**
+     * Tokenizes input from the sample input file,
+     * prints all of the tokens and errors in order,
+     * then prints the {@link BOOKKEEPER} it generated.
+     */
     private static void tokenizeTestScriptAndPrintTokensAndSymbolTable() {
         try (Reader testScriptReader = new BufferedReader(new InputStreamReader(getTokenizerTestTrumpScriptInputStream()))) {
             BOOKKEEPER symbolTable = new BOOKKEEPER();
@@ -37,6 +46,10 @@ public class TokenizerDemo {
         }
     }
 
+    /**
+     * Prints a of the lexeme->{@link Token} associations in the given {@link BOOKKEEPER}
+     * @param symbolTable a {@link BOOKKEEPER} containing lexeme->{@link Token} associations
+     */
     private static void printSymbolTable(BOOKKEEPER symbolTable) {
         System.out.println("Symbol table entries:");
         for (Token token : symbolTable.getEntries().values()) {
@@ -44,6 +57,11 @@ public class TokenizerDemo {
         }
     }
 
+    /**
+     * Uses the given {@link SCANNER} to get and then print all of the tokens provided by that {@link SCANNER}.
+     * @param tokenizerDFA a {@link SCANNER} that will provide tokens
+     * @throws IOException if there is a problem reading input
+     */
     private static void printAllTokensAndErrors(SCANNER tokenizerDFA) throws IOException {
         Token token;
         System.out.flush();
@@ -58,6 +76,9 @@ public class TokenizerDemo {
         }
     }
 
+    /**
+     * Echos the contents of the test script this demo uses, line for line.
+     */
     private static void echoContentsOfTestScript() {
         try (Reader testScriptReader = new BufferedReader(new InputStreamReader(getTokenizerTestTrumpScriptInputStream()))) {
             printContentsOfReader(testScriptReader);
@@ -69,10 +90,19 @@ public class TokenizerDemo {
         System.out.println();
     }
 
+    /**
+     * Gets a fresh handle to the test input file our professor gave us to test this project.
+     * @return a fresh handle to a sample TrumpScript++ file
+     */
     private static InputStream getTokenizerTestTrumpScriptInputStream() {
         return TokenizerDemo.class.getResourceAsStream(TOKENIZER_TEST_SCRIPT_RESOURCE_NAME);
     }
 
+    /**
+     * Prints every char provided by the given reader, until EOF is reached
+     * @param reader an opened input source
+     * @throws IOException if there's problem reading input
+     */
     private static void printContentsOfReader(Reader reader) throws IOException{
         int read;
         while((read = reader.read()) != -1){
