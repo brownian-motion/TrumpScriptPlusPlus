e9b3b8a271f5f7389747bc7b083a140d4c53cd80
JJ Brown <jack.brown.255@gmail.com>
2018-04-12 22:53:42 -0500

    Fixed visibility issues

--- src/test/java/com/brownian/trumpscript/parser/StackItemTypeTest.java
+++ src/test/java/com/brownian/trumpscript/parser/StackItemTypeTest.java
@@ -1,14 +1,11 @@
 package com.brownian.trumpscript.parser;
 
-import com.brownian.trumpscript.tokenizer.token.TokenType;
 import org.junit.jupiter.api.Test;
 
 import static com.brownian.trumpscript.parser.StackItemType.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class StackItemTypeTest {
+class StackItemTypeTest {
     private static StackItemType[] KEYWORDS = {
             MAKE,  // 4
             PROGRAMMING,
@@ -132,130 +129,130 @@ public class StackItemTypeTest {
                     ARITH,
                     ARITH_TAIL
             },
-    TERMINALS = {
-            //// Terminal tokens
-            ID, // 1
-            CONST, // 2
-            STRING, // 3
-            // keywords
-            MAKE,  // 4
-            PROGRAMMING,
-            GREAT,
-            AGAIN,
-            AMERICA,
-            IS,
-            ELSE,
-            NUMBER,
-            BOOLEAN,
-            IF,
-            AS,
-            LONG,
-            TELL,
-            SAY,
-            FACT,
-            LIE,
-            NOT,
-            AND,
-            OR,
-            LESS,
-            MORE,
-            PLUS,
-            TIMES, //26
-            // special symbols
-            COMMA, // 27
-            SEMICOLON,
-            COLON,
-            EXCLAMATION_MARK,
-            QUESTION_MARK,
-            LEFT_PAREN,
-            RIGHT_PAREN, // 33
-    },
-    NONTERMINALS = {
-            //// Nonterminals
-            TRUMP, // 34
-            FIRST,
-            LAST,
-            STMTS,
-            MORE_STMTS,
-            STMT,
-            DECL,
-            TYPE,
-            ASMT,
-            COND,
-            LOOP,
-            OUTPUT,
-            IDS,
-            MORE_IDS,
-            EXPR,
-            BOOL,
-            BOOL_TAIL,
-            TEST,
-            ARITH,
-            ARITH_TAIL
-    };
+            TERMINALS = {
+                    //// Terminal tokens
+                    ID, // 1
+                    CONST, // 2
+                    STRING, // 3
+                    // keywords
+                    MAKE,  // 4
+                    PROGRAMMING,
+                    GREAT,
+                    AGAIN,
+                    AMERICA,
+                    IS,
+                    ELSE,
+                    NUMBER,
+                    BOOLEAN,
+                    IF,
+                    AS,
+                    LONG,
+                    TELL,
+                    SAY,
+                    FACT,
+                    LIE,
+                    NOT,
+                    AND,
+                    OR,
+                    LESS,
+                    MORE,
+                    PLUS,
+                    TIMES, //26
+                    // special symbols
+                    COMMA, // 27
+                    SEMICOLON,
+                    COLON,
+                    EXCLAMATION_MARK,
+                    QUESTION_MARK,
+                    LEFT_PAREN,
+                    RIGHT_PAREN, // 33
+            },
+            NONTERMINALS = {
+                    //// Nonterminals
+                    TRUMP, // 34
+                    FIRST,
+                    LAST,
+                    STMTS,
+                    MORE_STMTS,
+                    STMT,
+                    DECL,
+                    TYPE,
+                    ASMT,
+                    COND,
+                    LOOP,
+                    OUTPUT,
+                    IDS,
+                    MORE_IDS,
+                    EXPR,
+                    BOOL,
+                    BOOL_TAIL,
+                    TEST,
+                    ARITH,
+                    ARITH_TAIL
+            };
 
     @Test
-    public void test_allTypesHaveNonEmptyToStrings() {
+    void test_allTypesHaveNonEmptyToStrings() {
         for (StackItemType type : StackItemType.values()) {
             assertNotEquals(type.toString(), "");
         }
     }
 
     @Test
-    public void test_allKeywordTypesAreKeywords() {
+    void test_allKeywordTypesAreKeywords() {
         for (StackItemType type : KEYWORDS) {
             assertTrue(type.isKeyword());
         }
     }
 
     @Test
-    public void test_allNonKeywordTypesAreNotKeywords() {
+    void test_allNonKeywordTypesAreNotKeywords() {
         for (StackItemType type : NOT_KEYWORDS) {
             assertFalse(type.isKeyword());
         }
     }
 
     @Test
-    public void test_allSpecialCharacterTypesAreSpecialCharacters() {
+    void test_allSpecialCharacterTypesAreSpecialCharacters() {
         for (StackItemType type : SPECIAL_SYMBOLS) {
             assertTrue(type.isSpecialSymbol());
         }
     }
 
     @Test
-    public void test_allNonSpecialCharacterTypesAreNotSpecialCharacters() {
+    void test_allNonSpecialCharacterTypesAreNotSpecialCharacters() {
         for (StackItemType type : NOT_SPECIAL_SYMBOLS) {
             assertFalse(type.isSpecialSymbol());
         }
     }
 
     @Test
-    public void test_allTerminalsAreTerminals(){
-        for(StackItemType type : TERMINALS){
+    void test_allTerminalsAreTerminals() {
+        for (StackItemType type : TERMINALS) {
             assertTrue(type.isTerminal());
             assertFalse(type.isNonterminal());
         }
     }
 
     @Test
-    public void test_allNonTerminalsAreNonTerminals(){
-        for(StackItemType type : NONTERMINALS){
+    void test_allNonTerminalsAreNonTerminals() {
+        for (StackItemType type : NONTERMINALS) {
             assertTrue(type.isNonterminal());
             assertFalse(type.isTerminal());
         }
     }
 
     @Test
-    public void test_stackBottomIsNeitherTerminalNorNonTerminal(){
+    void test_stackBottomIsNeitherTerminalNorNonTerminal() {
         assertFalse(STACK_BOTTOM_MARKER.isNonterminal());
         assertFalse(STACK_BOTTOM_MARKER.isTerminal());
     }
 
     @Test
-    public void test_onlyStackBottomIsStackBottom(){
+    void test_onlyStackBottomIsStackBottom() {
         assertTrue(STACK_BOTTOM_MARKER.isStackBottom());
         StackItemType[] values = StackItemType.values();
-        for(int i = 1 ; i < values.length ; i++){
+        for (int i = 1; i < values.length; i++) {
             assertFalse(values[i].isStackBottom());
         }
     }
--- src/test/java/com/brownian/trumpscript/tokenizer/TokenTypeTest.java
+++ src/test/java/com/brownian/trumpscript/tokenizer/TokenTypeTest.java
@@ -3,10 +3,10 @@ package com.brownian.trumpscript.tokenizer;
 import com.brownian.trumpscript.tokenizer.token.TokenType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static com.brownian.trumpscript.tokenizer.token.TokenType.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class TokenTypeTest {
+class TokenTypeTest {
     private static TokenType[] KEYWORDS = {
             MAKE,  // 4
             PROGRAMMING,
@@ -32,94 +32,94 @@ public class TokenTypeTest {
             PLUS,
             TIMES, //26
     },
-    NOT_KEYWORDS = {
-            MALFORMED_TOKEN, // 0 -- should not be used to select a StackItemType
-            //// Terminal tokens
-            ID, // 1
-            CONST, // 2
-            STRING, // 3
-            // special symbols
-            COMMA, // 27
-            SEMICOLON,
-            COLON,
-            EXCLAMATION_MARK,
-            QUESTION_MARK,
-            LEFT_PAREN,
-            RIGHT_PAREN // 33
-    },
-    SPECIAL_SYMBOLS = {
-            // special symbols
-            COMMA, // 27
-            SEMICOLON,
-            COLON,
-            EXCLAMATION_MARK,
-            QUESTION_MARK,
-            LEFT_PAREN,
-            RIGHT_PAREN // 33
-    },
-    NOT_SPECIAL_SYMBOLS = {
-            MALFORMED_TOKEN, // 0 -- should not be used to select a StackItemType
-            //// Terminal tokens
-            ID, // 1
-            CONST, // 2
-            STRING, // 3
-            // keywords
-            MAKE,  // 4
-            PROGRAMMING,
-            GREAT,
-            AGAIN,
-            AMERICA,
-            IS,
-            ELSE,
-            NUMBER,
-            BOOLEAN,
-            IF,
-            AS,
-            LONG,
-            TELL,
-            SAY,
-            FACT,
-            LIE,
-            NOT,
-            AND,
-            OR,
-            LESS,
-            MORE,
-            PLUS,
-            TIMES, //26
-    };
+            NOT_KEYWORDS = {
+                    MALFORMED_TOKEN, // 0 -- should not be used to select a StackItemType
+                    //// Terminal tokens
+                    ID, // 1
+                    CONST, // 2
+                    STRING, // 3
+                    // special symbols
+                    COMMA, // 27
+                    SEMICOLON,
+                    COLON,
+                    EXCLAMATION_MARK,
+                    QUESTION_MARK,
+                    LEFT_PAREN,
+                    RIGHT_PAREN // 33
+            },
+            SPECIAL_SYMBOLS = {
+                    // special symbols
+                    COMMA, // 27
+                    SEMICOLON,
+                    COLON,
+                    EXCLAMATION_MARK,
+                    QUESTION_MARK,
+                    LEFT_PAREN,
+                    RIGHT_PAREN // 33
+            },
+            NOT_SPECIAL_SYMBOLS = {
+                    MALFORMED_TOKEN, // 0 -- should not be used to select a StackItemType
+                    //// Terminal tokens
+                    ID, // 1
+                    CONST, // 2
+                    STRING, // 3
+                    // keywords
+                    MAKE,  // 4
+                    PROGRAMMING,
+                    GREAT,
+                    AGAIN,
+                    AMERICA,
+                    IS,
+                    ELSE,
+                    NUMBER,
+                    BOOLEAN,
+                    IF,
+                    AS,
+                    LONG,
+                    TELL,
+                    SAY,
+                    FACT,
+                    LIE,
+                    NOT,
+                    AND,
+                    OR,
+                    LESS,
+                    MORE,
+                    PLUS,
+                    TIMES, //26
+            };
 
     @Test
-    public void test_allTokensHaveNonEmptyToStrings(){
-        for (TokenType token : TokenType.values()){
+    void test_allTokensHaveNonEmptyToStrings() {
+        for (TokenType token : TokenType.values()) {
             assertNotEquals(token.toString(), "");
         }
     }
 
     @Test
-    public void test_allKeywordTokenTypesAreKeywords() {
-        for (TokenType token : KEYWORDS){
+    void test_allKeywordTokenTypesAreKeywords() {
+        for (TokenType token : KEYWORDS) {
             assertTrue(token.isKeyword());
         }
     }
 
     @Test
-    public void test_allNonKeywordTokenTypesAreNotKeywords() {
-        for (TokenType token : NOT_KEYWORDS){
+    void test_allNonKeywordTokenTypesAreNotKeywords() {
+        for (TokenType token : NOT_KEYWORDS) {
             assertFalse(token.isKeyword());
         }
     }
 
     @Test
-    public void test_allSpecialCharacterTypesAreSpecialCharacters(){
-        for(TokenType token : SPECIAL_SYMBOLS){
+    void test_allSpecialCharacterTypesAreSpecialCharacters() {
+        for (TokenType token : SPECIAL_SYMBOLS) {
             assertTrue(token.isSpecialSymbol());
         }
     }
 
     @Test
-    public void test_allNonSpecialCharacterTypesAreNotSpecialCharacters(){
-        for(TokenType token : NOT_SPECIAL_SYMBOLS){
+    void test_allNonSpecialCharacterTypesAreNotSpecialCharacters() {
+        for (TokenType token : NOT_SPECIAL_SYMBOLS) {
             assertFalse(token.isSpecialSymbol());
         }
     }
